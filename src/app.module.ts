ce44393363ceed1277751cb7b9225dc183a1fd25
Anas Khafagy <anas.khafagy@gen-c.com>
2025-01-10 20:14:20 +0200

    add event emitter lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DoctorAvailabilityModule } from './doctor-availability/doctor-availability.module';
+import { AppointmentConfirmationModule } from './appointment-confirmation/appointment-confirmation.module';
 
 @Module({
-  imports: [DoctorAvailabilityModule],
+  imports: [DoctorAvailabilityModule, AppointmentConfirmationModule],
   controllers: [],
   providers: [],
 })
